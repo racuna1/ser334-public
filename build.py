541471c3437dbb443691667f34fa2161213cba00
Ruben Acuna <racuna1@asu.edu>
2018-12-31 22:41:02 -0700

    Added PDF build system.

---
+++ build.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Builds PDF files for practice exams and sample problems (both blank and
+solution). Uses Lyx 2.3 with LuaTex internally. Results will be placed in
+build subdirectory.
+"""
+
+import os
+
+__author__      = "Ruben Acuna"
+__copyright__   = "Copyright 2019, Ruben Acuna"
+
+build_folder = os.sep + "build"
+cwd = os.getcwd()
+tex_toolchain = "pdf5" #LuaTex
+lyx_bin = "Lyx2.3"
+
+# preparation
+if not os.path.isdir(build_folder):
+    os.mkdir(build_folder)
+
+# build
+lyx_filenames = [x for x in os.listdir(".") if x.endswith(".lyx")]
+for lyx_filename in lyx_filenames:
+    blank_out_filename = cwd + build_folder + os.sep + lyx_filename[:-3] + "pdf"
+    soln_out_filename = cwd + build_folder + os.sep + lyx_filename[:-4] + "_soln.pdf"
+
+    # reference: https://wiki.lyx.org/FAQ/ImportExport
+    blank_para = lyx_bin + " -x \"command-sequence branch-activate blank; branch-deactivate soln\"" + " --export-to " + tex_toolchain + " " + blank_out_filename + " " + lyx_filename
+    soln_para = ("-x \"command-sequence branch-activate soln; branch-deactivate blank\" --export-to %s %s %s" % (tex_toolchain, soln_out_filename, lyx_filename))
+
+    cmd_blank = lyx_bin + " " + blank_para
+    os.system(cmd_blank)
+
+    if "exam" not in lyx_filename:
+        cmd_soln = lyx_bin + " " + soln_para
+        os.system(cmd_soln)
