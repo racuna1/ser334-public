c35b92b19b091474b4dd7cf702101bbef73edde2
Ruben Acuña <racuna1@asu.edu>
2021-10-28 23:15:48 -0700

    M08: updates from video.

--- code_samples/M07_MutexBasics/main.c
+++ code_samples/M07_MutexBasics/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>
 
 #define NUM_THREADS 10000
 
@@ -13,7 +12,8 @@ struct data_node {
 struct data_node* list_head = NULL;
 pthread_mutex_t list_lock;
 
-void addFront(int data) {
+//thread safe!
+void add_front(int data) {
     struct data_node* node = (struct data_node*)malloc(sizeof(struct data_node));
 
     node->data = data;
@@ -28,26 +28,19 @@ void display_list() {
     struct data_node* iter = list_head;
     int count = 0;
 
-    while(iter != NULL ){
+    while(iter != NULL) {
         printf("Node: data=%d\n", iter->data);
         count++;
         iter = iter->next;
     }
 
-    printf("Node Count: %d", count);
+    printf("Node Count: %d\n", count);
 }
 
 void* runner_create_arg_node(void* arg) {
-
     int data = (int)arg;
 
-    struct timespec wait_time;
-    wait_time.tv_sec = 0;
-
-    wait_time.tv_nsec = rand() % 100000 + 100000;
-    nanosleep(&wait_time, NULL);
-
-    addFront(data);
+    add_front(data);
 
     pthread_exit(0);
 }
@@ -55,12 +48,10 @@ void* runner_create_arg_node(void* arg) {
 int main() {
     pthread_t tids[NUM_THREADS];
 
-    srand(time(NULL));
-
     pthread_mutex_init(&list_lock, NULL);
 
     for(int i = 0; i < NUM_THREADS; i++)
-        pthread_create(&tids[i], NULL, runner_create_arg_node, (void *) i);
+        pthread_create(&tids[i], NULL, runner_create_arg_node, (void*) i);
 
     for(int i = 0; i < NUM_THREADS; i++)
         pthread_join(tids[i], NULL);
